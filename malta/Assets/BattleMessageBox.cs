150000c946b68980417bf2527c08946b50ce44c2
Afti <yodamanyula@gmail.com>
2016-12-07 13:14:21 -0500

    Battle UI is still a work in progress, but it at least does communicate what's happening now!

---
+++ malta/Assets/BattleMessageBox.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public enum BattleMessageType
+{
+    StandardTurnMessage,
+    Silence,
+    ShieldWall,
+    ShieldBlock,
+    Heal,
+    Haste,
+    Barrier,
+    MultiHeal,
+    Encore,
+    Feedback,
+    Revenge,
+    Critical,
+    SavedAlly,
+    Flanking,
+    SomebodyDead,
+    Win,
+    Loss
+}
+
+public class BattleMessageBox : MonoBehaviour
+{
+    public BattleOverseer overseer;
+    public Text uiText;
+    public string line0;
+    public string line1;
+    public TextAsset mainStringsResource;
+    public TextAsset actionNameStringsResource;
+    public Queue<Battler> corpseQueue;
+    private string[] mainStrings;
+    private string[] actionNameStrings;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mainStrings = mainStringsResource.text.Split('\n');
+        actionNameStrings = actionNameStringsResource.text.Split('\n');
+        corpseQueue = new Queue<Battler>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Step(BattleMessageType message = BattleMessageType.StandardTurnMessage)
+    {
+        bool drawToLine1 = true;
+        if (line0 == "") drawToLine1 = false;
+        else if (line1 != "") line0 = line1;
+        Debug.Log(drawToLine1);
+        string baseLine;
+        string nextLine = "";
+        switch (message)
+        {
+            case BattleMessageType.StandardTurnMessage:
+                if (overseer.standardActionPriorityBracket) baseLine = mainStrings[0];
+                else baseLine = mainStrings[1];
+                if (overseer.currentActingBattler.isEnemy) nextLine = overseer.currentActingBattler.adventurer.title + baseLine + actionNameStrings[(int)overseer.nextAction];
+                else nextLine = overseer.currentActingBattler.adventurer.fullName + baseLine + actionNameStrings[(int)overseer.nextAction];
+                break;
+            case BattleMessageType.Silence:
+                nextLine = mainStrings[3];
+                break;
+            case BattleMessageType.ShieldWall:
+                nextLine = mainStrings[4];
+                break;
+            case BattleMessageType.ShieldBlock:
+                nextLine = mainStrings[5];
+                break;
+            case BattleMessageType.Heal:
+                nextLine = mainStrings[6];
+                break;
+            case BattleMessageType.Haste:
+                nextLine = mainStrings[7];
+                break;
+            case BattleMessageType.Barrier:
+                nextLine = mainStrings[8];
+                break;
+            case BattleMessageType.MultiHeal:
+                nextLine = mainStrings[9];
+                break;
+            case BattleMessageType.Encore:
+                nextLine = mainStrings[10];
+                break;
+            case BattleMessageType.Feedback:
+                nextLine = mainStrings[11];
+                break;
+            case BattleMessageType.Revenge:
+                nextLine = mainStrings[12];
+                break;
+            case BattleMessageType.Critical:
+                nextLine = mainStrings[13];
+                break;
+            case BattleMessageType.SavedAlly:
+                nextLine = mainStrings[14];
+                break;
+            case BattleMessageType.Flanking:
+                nextLine = mainStrings[15];
+                break;
+            case BattleMessageType.SomebodyDead:
+                Battler bat = corpseQueue.Dequeue();
+                if (bat.isEnemy) nextLine = bat.adventurer.title + mainStrings[2];
+                else nextLine = bat.adventurer.fullName + mainStrings[2];
+                break;
+            case BattleMessageType.Win:
+                nextLine = mainStrings[16];
+                break;
+            case BattleMessageType.Loss:
+                nextLine = mainStrings[17];
+                break;
+        }
+        if (drawToLine1) line1 = nextLine;
+        else line0 = nextLine;
+        uiText.text = line0 + System.Environment.NewLine + line1;
+    }
+}
---
+++ malta/Assets/Scripts/BattleDamageNumbersGadget.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleDamageNumbersGadget : MonoBehaviour
+{
+    public Text uiText;
+    public Direction direction;
+    public float moveDist;
+    public float lifespan;
+    private float timeAlive;
+    private Color originalColor;
+    private Vector3 originalPosition;
+    private bool triggeredGadget = false;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        originalColor = uiText.color;
+        originalPosition = transform.position;
+        uiText.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (triggeredGadget)
+        {
+            timeAlive += Time.deltaTime;
+            switch (direction)
+            {
+                case Direction.Down:
+                    transform.position = transform.position + (Vector3.down * moveDist * Time.deltaTime);
+                    break;
+                case Direction.DownLeft:
+                    transform.position = transform.position + (((Vector3.down + Vector3.left) / 2) * moveDist * Time.deltaTime);
+                    break;
+                case Direction.DownRight:
+                    transform.position = transform.position + (((Vector3.down + Vector3.right) / 2) * moveDist * Time.deltaTime);
+                    break;
+                case Direction.Up:
+                    transform.position = transform.position + (Vector3.up * moveDist * Time.deltaTime);
+                    break;
+                case Direction.UpLeft:
+                    transform.position = transform.position + (((Vector3.up + Vector3.left) / 2) * moveDist * Time.deltaTime);
+                    break;
+                case Direction.UpRight:
+                    transform.position = transform.position + (((Vector3.up + Vector3.right) / 2) * moveDist * Time.deltaTime);
+                    break;
+                case Direction.Left:
+                    transform.position = transform.position + (Vector3.left * moveDist * Time.deltaTime);
+                    break;
+                case Direction.Right:
+                    transform.position = transform.position + (Vector3.right * moveDist * Time.deltaTime);
+                    break;
+            }
+            uiText.color = Color.Lerp(originalColor, Color.clear, timeAlive / lifespan);
+            if (timeAlive > lifespan)
+            {
+                uiText.enabled = false;
+                triggeredGadget = false;
+            }
+        }
+	}
+
+    public void Trigger(int dmg)
+    {
+        uiText.enabled = true;
+        uiText.text = dmg.ToString();
+        uiText.color = originalColor;
+        transform.position = originalPosition;
+        timeAlive = 0;
+        triggeredGadget = true;
+    } 
+}
